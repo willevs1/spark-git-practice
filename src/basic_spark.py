28d851ea734dc6de07d5a33ccd811c6ac4499b0f
Will <willevans@MacBookPro.communityfibre.co.uk>
2026-01-05 16:12:30 +0000

    New Path

--- src/basic_spark.py
+++ src/basic_spark.py
@@ -1,15 +1,18 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import avg
+import os
 
 spark = SparkSession.builder.appName("Test").getOrCreate()
 
+base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+data_path = os.path.join(base_path, "data", "sample.csv")
+
 df = spark.read.option("header", True).option("inferSchema", True) \
-    .csv("../data/sample.csv")
+    .csv(data_path)
 
 df.show()
 
 df.groupBy("dept").agg(avg("salary").alias("avg_salary")).show()
 
-change = 1 
-
 spark.stop()
+
